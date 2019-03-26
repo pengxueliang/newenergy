706c320dbd0f328b14c3ac7c6f909986eb73f2fe
zenghui <714676641@qq.com>
2019-03-26 20:55:45 +0800

    Update TestController.java

--- newenergy-admin-api/src/main/java/newenergy/admin/controller/TestController.java
+++ newenergy-admin-api/src/main/java/newenergy/admin/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    //从远程仓库dev分支pull下来的最新版本
     @RequestMapping("/test")
     public String hello(){
         return "hello world123";
