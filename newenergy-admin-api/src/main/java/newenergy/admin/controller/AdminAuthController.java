2beb7565ba07a6f22c84eb49c46e89429096f2c2
kekeyouxing <844765548@qq.com>
2019-03-29 20:24:00 +0800

    update dashboard

--- newenergy-admin-api/src/main/java/newenergy/admin/controller/AdminAuthController.java
+++ newenergy-admin-api/src/main/java/newenergy/admin/controller/AdminAuthController.java
@@ -2,17 +2,24 @@ package newenergy.admin.controller;
 
 import newenergy.core.util.JacksonUtil;
 import newenergy.core.util.ResponseUtil;
+import newenergy.db.domain.NewenergyAdmin;
+import newenergy.db.service.NewenergyAdminService;
+import newenergy.db.service.NewenergyPermissionService;
+import newenergy.db.service.NewenergyRoleService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.authc.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import static newenergy.admin.util.AdminResponseCode.ADMIN_INVALID_ACCOUNT;
 
@@ -20,6 +27,14 @@ import static newenergy.admin.util.AdminResponseCode.ADMIN_INVALID_ACCOUNT;
 @RequestMapping("/admin/auth")
 public class AdminAuthController {
 
+    @Autowired
+    private NewenergyAdminService adminService;
+
+    @Autowired
+    private NewenergyRoleService roleService;
+
+    @Autowired
+    private NewenergyPermissionService permissionService;
     @PostMapping("/login")
     public Object login(@RequestBody String body){
         String username = JacksonUtil.parseString(body, "username");
@@ -40,6 +55,33 @@ public class AdminAuthController {
             return ResponseUtil.fail(ADMIN_INVALID_ACCOUNT, ae.getMessage());
         }
         return ResponseUtil.ok(currentUser.getSession().getId());
+
+//        NewenergyAdmin admin = (NewenergyAdmin)adminService.findAdmin("admin123");
+//        admin.setRoleIds(new Integer[]{1});
+//        admin.setUsername("admin");
+//        adminService.save(admin);
+///    return ResponseUtil.ok();
     }
 
+    @GetMapping("/401")
+    public Object page401(){
+        return ResponseUtil.unlogin();
+    }
+
+    @RequiresAuthentication
+    @GetMapping("/info")
+    public Object info(){
+        Subject currentUser = SecurityUtils.getSubject();
+        NewenergyAdmin admin = (NewenergyAdmin)currentUser.getPrincipal();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", admin.getUsername());
+
+        Integer[] roleIds = admin.getRoleIds();
+        Set<String> roles = roleService.queryByIds(roleIds);
+        Set<String> permissions = permissionService.queryByRoleIds(roleIds);
+        data.put("roles", roles);
+        data.put("perms", permissions);
+        return ResponseUtil.ok(data);
+    }
 }
